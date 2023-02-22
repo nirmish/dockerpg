5627f12cb494bcaa968000224687e21d703415b9
Nirmish <59681644+nirmish@users.noreply.github.com>
2023-02-22 11:59:15 +0530

    Update MyDemoController.java

--- src/main/java/com/nirmish/pg/docker/demo/controller/MyDemoController.java
+++ src/main/java/com/nirmish/pg/docker/demo/controller/MyDemoController.java
@@ -10,7 +10,7 @@ public class MyDemoController {
 
 	@GetMapping("/hello")
 	public String getMessage() {
-		return "From : My Demo App -- Weolcome";
+		return "From : My Demo App -- Weolcome!!!!";
 	}
 	@GetMapping("/bye")
 	public String getBye() {
